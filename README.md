7183086f30a1d2c32093ae75a29cc9c61042a575
Shaurya Mishra <shaurya.mishra@gdn-commerce.com>
2022-06-20 19:45:17 +0530

    Add files & update README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# billingCalculator
-Tool to iterate over variable price slabs and calculate electricity bill
+# Billing Calculator
+
+Implement a bill calculator provided the number of units consumed and the slab information.
+
+
+## Example Slabs
+
+```
+1 to 100 units charged at Rs.1 per unit
+101 to 200 units charged at Rs.2 per unit
+201 to 300 units charged at Rs.3 per unit
+300 and above unit charged at Rs5 per unit
+ 
+Sample bill calculation formula for an input of 250 units
+(100 units * Rs.1) + (100 units * Rs.2) + (50 units * Rs.3) = Rs.450
+```
+
---
+++ billingCalculator.py
@@ -0,0 +1,52 @@
+#Function to determine bill by iterating over all slabs
+def billCalculator(slabsDictionaryList,totalUnits):
+    billAmount = 0
+    for slab in slabsDictionaryList:
+        #if totalUnits is greater than highest of slab -- cost for that slab is calculated, then loop iterates to next slab
+        #if it is the highest slab (as shown by -1) or totalUnits is less than highest of slab -- cost for that slab is calculated, then loop is exited
+        if totalUnits>slab['high']:
+            if slab['high']==-1:
+                billAmount+=(totalUnits-slab['low'])*slab['unitCost']
+                break
+            else:
+                billAmount+=(slab['high']-slab['low']+1)*slab['unitCost']
+        else:
+            billAmount+=(totalUnits-slab['low']+1)*slab['unitCost']
+            break
+    return billAmount
+
+#Function to read input from billingInput.txt and call secondary function to create dictionaries
+def inputFileReader():
+    #mention path to input file below
+    with open('billingInput.txt') as inputFile:
+        slabs = inputFile.readlines()
+        slabs = [slab.rstrip() for slab in slabs]
+    slabsDictionaryList = createDictionaryList(slabs)
+    return slabsDictionaryList
+
+#Function to create dictionary per slab and append them to a list
+#Also gets values of totalUnits from billingInput.txt file
+def createDictionaryList(slabs):
+    totalUnits = 0
+    slabDictionaryList=[]
+    for slab in slabs:
+        if "Total Units:" in slab:
+            totalUnits = int(slab.replace('Total Units: ','').strip())
+            continue
+        slabArray = slab.replace(',','').split(' ')
+        slabDictionary = {"low":int(slabArray[0]),"high":int(slabArray[1]),"unitCost":int(slabArray[2])}
+        slabDictionaryList.append(slabDictionary)
+    #totalUnits is the number of units consumed
+    #slabDictionaryList is an array containing one dictionary per slab
+    #slabDictionaryList --> [{"low":x,"high":y,:"value":z},{"low":x,"high":y,:"value":z},{"low":x,"high":y,:"value":z}...]
+    return totalUnits,slabDictionaryList
+
+
+def main():
+    totalUnits,slabsDictionaryList = inputFileReader()
+    totalUnitsCopy=totalUnits
+    billAmount = billCalculator(slabsDictionaryList,totalUnits)
+    print("Bill is {} for {} units".format(billAmount,totalUnitsCopy))
+
+if __name__ == "__main__":
+    main()
